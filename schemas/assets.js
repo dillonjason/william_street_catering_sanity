01a2f51038f3ee19a46db76c4d454ec65b1c5144
Dillon Jason <djason@thefarmersdog.com>
2020-02-16 10:29:25 -0500

    Added slug to assets

--- schemas/assets.js
+++ schemas/assets.js
@@ -6,7 +6,8 @@ export default {
     {
       name: "name",
       title: "Name",
-      type: "string"
+      type: "string",
+      validation: Rule => Rule.required()
     },
     {
       name: "image",
@@ -14,7 +15,18 @@ export default {
       type: "image",
       options: {
         hotspot: true
-      }
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96
+      },
+      validation: Rule => Rule.required()
     }
   ],
   preview: {
